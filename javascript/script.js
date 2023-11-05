013147a58158cf6988840cd0988906a4f7f2ceff
AliTaskiran <zeynelali-06@hotmail.com>
2023-11-05 12:08:58 +0300

    HTML-CSS-JAVASCRİPT

---
+++ javascript/script.js
@@ -0,0 +1,16 @@
+var urunAdi = "iphone 13"; //string
+let urunFiyat = 3000; // number
+
+console.log(typeof urunAdi);
+console.log(typeof urunFiyat);
+
+let sayi1 = 10;
+let sayi2 = 20;
+
+console.log(sayi1.toString() + sayi2.toString());
+let isim = "sena";
+let soyad = "Turan";
+console.log(isim + " " + soyad);
+let sinavNotu = 60;
+let basariliMi = sinavNotu >= 50;
+console.log(basariliMi);
---
+++ javascript/variables-demo.js
@@ -0,0 +1,6 @@
+var ogrenci1Ad = "Ada";
+var ogrenci1Soyad = "Bilgi";
+var ogrenci1Dogum = 2012;
+var matNotlari1 = 50;
+var matNotlari2 = 60;
+var matNotlari3 = 70;
